97f6e14aed8f5ec11b2e89ef8306a8b3d488af8a
unknown <sahilrawat049@gmail.com>
2025-02-17 15:36:04 +0530

    url changes

--- pdf_reader/urls.py
+++ pdf_reader/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FileChatView
 
 urlpatterns = [
-    path('json-coverter/', FileChatView.as_view(), name='chat_with_file'),
+    path('json-converter/', FileChatView.as_view(), name='chat_with_file'),
 ]
